8e10546840789d62e01e32b3cca9611f75f9fcc2
Owen Innes <me@oweninnes.com>
2017-10-23 23:33:12 +1100

    Changed Template

---
+++ public/js/slider-2.js
@@ -0,0 +1,98 @@
+/*! --------------------------------------------------------------
+# slider-2.js
+#
+# Main theme js file for SaltTechno template.
+# This is compressed js file. You get uncompressed version with download.
+--------------------------------------------------------------*/
+
+$(function($) {
+    'use strict';
+
+    var tpj=jQuery;
+
+    var revapi21;
+    tpj(document).ready(function() {
+        if(tpj("#rev_slider_21_1").revolution == undefined){
+            revslider_showDoubleJqueryError("#rev_slider_21_1");
+        }else{
+            revapi21 = tpj("#rev_slider_21_1").show().revolution({
+                sliderType:"hero",
+                sliderLayout:"fullscreen",
+                dottedOverlay:"none",
+                delay:20000,
+                navigation: {
+                },
+                responsiveLevels:[1240,1024,778,778],
+                visibilityLevels:[1240,1024,778,778],
+                gridwidth:[1240,1024,778,480],
+                gridheight:[600,500,400,300],
+                lazyType:"none",
+                parallax: {
+                    type:"mouse",
+                    origo:"slidercenter",
+                    speed:2000,
+                    levels:[2,3,4,5,6,7,12,16,10,50,46,47,48,49,50,55],
+                    type:"mouse",
+                },
+                shadow:0,
+                spinner:"off",
+                autoHeight:"off",
+                fullScreenAutoWidth:"off",
+                fullScreenAlignForce:"off",
+                fullScreenOffsetContainer: "",
+                fullScreenOffset: "60px",
+                disableProgressBar:"on",
+                hideThumbsOnMobile:"off",
+                hideSliderAtLimit:0,
+                hideCaptionAtLimit:0,
+                hideAllCaptionAtLilmit:0,
+                debugMode:false,
+                fallbacks: {
+                    simplifyAll:"off",
+                    disableFocusListener:false,
+                }
+            });
+        // CHANGE THE API REFERENCE, AND THE ELEMENTS YOU WISH TO BLUR / UNBLUR
+        // SET START BLUR FACTOR, END BLUR FACTOR AND
+
+        var api = revapi21,
+        ElementsToBlur = api.find('.toblur.tp-caption'),
+        ElementsToUnBlur = api.find('.tounblur.tp-caption'),
+        UnBlurFactor = 2,
+        UnBlurStart = 3,
+        UnBlurEnd = 0,
+        BlurStart = 0,
+        BlurEnd = 5,
+        BlurFactor = 2,
+        blurCall = new Object();
+
+
+        // SOME CODE FOR BLUR AND UNBLUR ELEMENTS
+        // EXTEND THE REVOLUTION SLIDER FUNCTION
+        // CHANGE ONLY IF YOU KNOW WHAT YOU DO
+
+        blurCall.inmodule = "parallax";
+        blurCall.atposition = "start";
+        blurCall.callback = function() {
+        var proc = api.revgetparallaxproc(),
+          blur = UnBlurStart+(proc*UnBlurStart*UnBlurFactor)+UnBlurEnd,
+          nblur = Math.abs(proc*BlurEnd*BlurFactor)+BlurStart;
+
+        blur = blur<UnBlurEnd?UnBlurEnd:blur;
+        nblur = nblur>BlurEnd?BlurEnd:nblur;
+
+        ElementsToUnBlur = jQuery(ElementsToUnBlur.selector);
+        punchgs.TweenLite.set(ElementsToUnBlur,{'-webkit-filter':'blur('+(blur)+'px)', 'filter':'blur('+(blur)+'px)'});
+        punchgs.TweenLite.set(ElementsToBlur,{'-webkit-filter':'blur('+(nblur)+'px)', 'filter':'blur('+(nblur)+'px)'});
+        }
+
+        api.bind("revolution.slide.layeraction",function (e) {
+        blurCall.callback();
+        });
+
+        api.bind("revolution.slide.onloaded",function (e) {
+        revapi21.revaddcallback(blurCall);
+        });				}
+    });	/*ready*/
+
+});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,97 +1,299 @@
-<!DOCTYPE html>
+<!doctype html>
 <html class="no-js" lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>The Startup Agency</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta name="author" content="weibergmedia">
-<meta name="Description" content="Three – Responsive HTML5 Portfolio for Creatives" />
-<link href="css/reset.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="css/contact.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/styles.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/responsive.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="http://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet" type="text/css" />
-<link href="http://fonts.googleapis.com/css?family=Lora:400,400italic,600" rel="stylesheet" type="text/css" />
-<script src="js/modernizr.js" type="text/javascript"></script>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Areen - Perfect HTML template for your business</title>
+
+	<!-- ========== META TAGS ========== -->
+	<meta name="description" content="Options - Responsive Multipurpose HTML Template ">
+	<meta name="keywords" content="business, responsive, multi-purpose">
+	<meta name="author" content="SaltTechno">
+
+	<!-- ========== ICONS ========== -->
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<!-- Place favicon.ico in the root directory -->
+
+	<!-- ========== COMMON STYLES ========== -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+	<link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+	<link rel="stylesheet" href="css/ionicons.min.css" media="screen">
+	<link rel="stylesheet" href="css/animate.min.css" media="screen">
+	<link rel="stylesheet" href="css/animsition.min.css" media="screen">
+
+	<!-- ========== PAGE STYLES ========== -->
+	<link rel="stylesheet" type="text/css" href="css/revslider/settings.css">
+	<link rel="stylesheet" href="css/revslider/layers.css" media="screen">
+	<link rel="stylesheet" href="css/revslider/navigation.css" media="screen">
+	<link rel="stylesheet" href="css/owl/owl.carousel.min.css" media="screen">
+	<link rel="stylesheet" href="css/owl/owl.theme.default.min.css" media="screen">
+
+	<!-- ========== THEME CSS ========== -->
+	<link rel="stylesheet" href="css/main.min.css" media="screen" >
+
+	<!-- ========== MODERNIZR ========== -->
+	<script src="js/modernizr/modernizr.min.js"></script>
 </head>
-<body class="full-height">
-<!-- Preloader -->
-<div id="preloader">
-  <div id="status">
-    <div class="parent">
-      <div class="child">
-        <p>loading</p>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- start main nav -->
-<nav id="main-nav">
-  <ul id="options" class="clearfix">
-    <li> <a href="{{ route('home') }}" class="{{ (Route::currentRouteNamed('home')) ? 'selected' : '' }}">Home</a> </li>
-    <li> <a href="{{ route('portfolio') }}" class="{{ (Route::currentRouteNamed('portfolio')) ? 'selected' : '' }}">Portfolio</a></li>
-    <li> <a href="{{ route('services') }}" class="{{ (Route::currentRouteNamed('services')) ? 'selected' : '' }}">Services</a> </li>
-    <li> <a href="{{ route('agency') }}" class="{{ (Route::currentRouteNamed('agency')) ? 'selected' : '' }}">Agency</a> </li>
-    <li> <a href="{{ route('blog') }}" class="{{ (Route::currentRouteNamed('blog')) ? 'selected' : '' }}">Blog</a> </li>
-    <li> <a href="#" class="sub-nav-toggle">Features</a>
-      <ul class="sub-nav">
-        <li> <a href="work.html">Project</a> </li>
-        <li> <a href="slider.html">Slider</a> </li>
-        <li> <a href="post.html">Post</a> </li>
-        <li> <a href="404.html">404</a> </li>
-      </ul>
-    </li>
-    <li> <a href="{{ route('contact') }}" class="{{ (Route::currentRouteNamed('contact')) ? 'selected' : '' }}">Contact</a> </li>
-  </ul>
-  <ul class="social-list clearfix">
-    <li> <a href="#"><i class="fa fa-twitter"></i></a> </li>
-    <li> <a href="#"><i class="fa fa-pinterest-p"></i></a> </li>
-    <li> <a href="#"><i class="fa fa-instagram"></i></a> </li>
-    <li> <a href="#"><i class="fa fa-google-plus"></i></a> </li>
-  </ul>
-</nav>
-<!-- end main nav -->
-<div id="wrap">
-  <header id="header">
-    <div class="logo-wrapper">
-      <h1 id="logo"><a href="{{ route('home') }}">Three</a></h1>
-      <div class="tagline"><span>The <br / />Startup <br />
-        Agency</span></div>
-    </div>
-    <div id="menu-button">
-      <div class="centralizer">
-        <div class="menu-trigger"></div>
-        <div class="cursor">
-          <div id="nav-button"> <span class="nav-bar"></span> <span class="nav-bar"></span> <span class="nav-bar"></span>
-            <div class="circle-anim"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
-  <!-- end header -->
+
+<body>
+
+	<div class="main-wrapper animsition">
+		<nav class="navbar me-menu navbar-fixed-top">
+			<div class="container-fluid">
+
+				<!-- ========== BRAND & TOGGLE - KEPT TOGETHER FOR BETTER MOBILE DISPLAY ========== -->
+				<div class="navbar-header">
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#me-navbar" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.html">
+						<img src="images/logo.png" alt="logo">
+					</a>
+				</div>
+				<!-- /.navbar-header -->
+
+				<!-- ========== NAV LINKS & ITS CONTENT ========== -->
+				<div class="collapse navbar-collapse" id="me-navbar">
+					<ul class="nav navbar-nav pull-right">
+						<li class="active dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Home <span class="caret"></span></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="index.html">Home Default</a></li>
+												<li><a href="index-2.html">Business Home 2</a></li>
+												<li><a href="index-3.html">One Page Home</a></li>
+												<li><a href="index-4.html">Business Home 4</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+							<!-- / .dropdown-menu -->
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Us <b class="caret"></b></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="about-modern.html">About Modern</a></li>
+												<li><a href="about-classic.html">About Classic</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+										</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+							<!-- / .dropdown-menu -->
+						</li>
+						<!-- / .dropdown (features) -->
+						<li class="dropdown dd-left">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <b class="caret"></b></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="services-modern.html">Services Modern</a></li>
+												<li><a href="services-grid.html">Services Grid</a></li>
+												<li><a href="services-detail.html">Single Service Details</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+							<!-- / .dropdown-menu -->
+						</li>
+						<li class="dropdown dd-left">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pricing <span class="caret"></span></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="pricing-modern.html">Pricing Modern</a></li>
+												<li><a href="pricing-classic.html">Pricing Classic</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown dd-left">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog <span class="caret"></span></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="blog-modern.html">Blog Home Modern</a></li>
+												<li><a href="blog-classic.html">Blog Home Classic</a></li>
+												<li><a href="blog-post.html">Blog Single Post</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown dd-left">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shortcodes <span class="caret"></span></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="shortcodes-accordions.html">Accordions</a></li>
+												<li><a href="shortcodes-blockquote.html">Blockquotes</a></li>
+												<li><a href="shortcodes-charts.html">Charts</a></li>
+												<li><a href="shortcodes-columns-grids.html">Column Grid</a></li>
+												<li><a href="shortcodes-tables.html">Tables</a></li>
+												<li><a href="shortcodes-tabs.html">Tabs</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown dd-left">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact Us <span class="caret"></span></a>
+							<ul class="dropdown-menu animated fadeIn">
+								<li>
+									<div class="me-menu-content">
+										<div class="nav-col">
+											<ul>
+												<li><a href="contact-modern.html">Contact - Modern</a></li>
+												<li><a href="contact-classic.html">Contact - Classic</a></li>
+											</ul>
+										</div>
+										<!-- / .nav-col -->
+									</div>
+									<!-- / .me-menu-content -->
+								</li>
+							</ul>
+						</li>
+						<!-- / .dropdown.dd-left (pages) -->
+					</ul>
+					<!-- /.nav navbar-nav -->
+				</div>
+				<!-- /#me-navbar.collapse navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+		<!-- /.navbar navbar-default -->
 
   @yield('content')
 
 
-  <footer id="footer" class="clearfix">
-    <div class="padding-wrapper">
-      <p class="alignleft">© 2017, The Startup Agency.</p>
-      <p class="alignright">Made with <i class="fa fa-heart"></i> in Australia.</p>
+  <!-- ========== FOOTER ========== -->
+  <footer class="footer">
+    <div class="container top-container">
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <h6>About Us</h6>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <!-- /.col-md-4 -->
+
+        <div class="col-md-4 col-sm-6">
+          <h6>Contact Us</h6>
+          <p><i class="ion-ios-location"></i> A902, Brighton, East Sussex, England.</p>
+          <p><i class="ion-email"></i> support@themesease.com</p>
+          <p><i class="ion-ios-telephone"></i> +1 (123) 456-7890</p>
+        </div>
+        <!-- /.col-md-4 col-sm-6 -->
+
+        <div class="col-md-4 col-sm-12">
+          <h6>Follow Us</h6>
+          <a href="#" class="social-link"><i class="ion-social-facebook"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-twitter"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-instagram"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-whatsapp"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-googleplus"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-dribbble"></i></a>
+          <a href="#" class="social-link"><i class="ion-social-tumblr"></i></a>
+          <a href="https://themeforest.net/cart/configure_before_adding/18016228?license=regular&ref=salttechno&size=source&support=bundle_12month" class="btn st-btn btn-rounded small-btn mt10">Purchase Now</a>
+        </div>
+        <!-- /.col-md-4 col-sm-6 -->
+      </div>
+      <!-- /.row -->
     </div>
+    <!-- /.container -->
+
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <p class="col-sm-6">&copy; Salt Technologies  | 2016</p>
+          <div class="col-sm-6 text-right">
+            <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.footer-bottom -->
   </footer>
-  <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
-  <script src="js/jquery-easing-1.3.js" type="text/javascript"></script>
-  <script src="js/jquery.touchSwipe.min.js" type="text/javascript"></script>
-  <script src="js/main.js" type="text/javascript"></script>
-  <script src="js/jquery.form.js" type="text/javascript"></script>
-  <script src="js/input.fields.js" type="text/javascript"></script>
-  <script src="js/preloader.js" type="text/javascript"></script>
-  <script src="js/jquery.fancybox.pack.js" type="text/javascript"></script>
-  <script src="js/bootstrap.min.js" type="text/javascript"></script>
-  <script src="js/jquery.mixitup.min.js" type="text/javascript"></script>
-  </body>
-  </html>
+  <!-- /.footer -->
+</div>
+<!-- /.main-wrapper -->
+
+<!-- ========== LOADER ========== -->
+  <div class="page-loader">
+      <div class="spinner-2"></div>
+  </div>
+
+<!-- ========== COMMON JS FILES ========== -->
+<script src="js/jquery/jquery-2.2.4.min.js"></script>
+<script src="js/bootstrap/bootstrap.min.js"></script>
+
+<!-- ========== PAGE JS FILES ========== -->
+<!-- REVOLUTION JS FILES -->
+<script type="text/javascript" src="js/revslider/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="js/revslider/jquery.themepunch.revolution.min.js"></script>
+
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/animsition/animsition.min.js"></script>
+
+<!-- ========== THEME JS ========== -->
+<script src="js/main.min.js"></script>
+<script src="js/slider-2.min.js"></script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-84485047-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
